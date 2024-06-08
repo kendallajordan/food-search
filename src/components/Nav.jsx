4a670709d2ea0b4ae5d48956055668c4fcc092ad
Kendall Jordan <kendallajordan@gmail.com>
2024-06-07 23:32:16 -0700

    edited title

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,5 +1,5 @@
 import styles from "./nav.module.css";
 
 export default function Nav() {
-  return <div className={styles.nav}>🍕 foodsearch</div>;
+  return <div className={styles.nav}>🍕 Food Search</div>;
 }
